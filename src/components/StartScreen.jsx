3f6c75cf832544ddd7705c08510da1f6763ddf5d
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-08 11:35:54 -0400

    Imported Props for state

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import blob from '../assets/blobs@2x.png'
 import blob2 from "../assets/blobs.png"
-export default function StartScreen(){
+export default function StartScreen(props){
+    
 return(
     <>
     <h1>Quizzical</h1>
     <h4>Test Your Knowledge</h4>
-    <button type="button">Start Quiz</button>
+    <button type="button" onClick={props.handleClick}>Start Quiz</button>
     </>
 )
 
