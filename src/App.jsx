f8c82afd59cbdfa993d4aac86c30c9843e5d1380
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-08 10:38:23 -0400

    Created StartScreen.jsx to house intro screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-
+import Start from "./components/StartScreen"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Start/>
       <h1>Test</h1>
     </div>
   )
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+export default function StartScreen(){
+return(
+
+    <h1>Hi</h1>
+
+)
+
+
+
+
+
+
+}
