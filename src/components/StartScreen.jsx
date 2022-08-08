a5cc439523b51f979d17e66f616495d7abf8e8cc
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-08 10:55:05 -0400

    Imported Font + Img 2

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import blob from '../assets/blobs@2x.png'
-import blob2 from "../assets/blob2"
+import blob2 from "../assets/blobs.png"
 export default function StartScreen(){
 return(
     <>
