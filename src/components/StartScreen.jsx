57e7593a5171eba9e29317160c68342d47c04da2
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-08 10:48:30 -0400

    Setup StartScreen html

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,11 +1,15 @@
 import React from "react"
-
-
+import blob from '../assets/blobs@2x.png'
+import blob2 from "../assets/blob2"
 export default function StartScreen(){
 return(
-
-    <h1>Hi</h1>
-
+    <>
+    <img src={blob}></img>
+    <h1>Quizzical</h1>
+    <h4>Test Your Knowledge</h4>
+    <button type="button">Start Quiz</button>
+    <img src={blob2}></img>
+    </>
 )
 
 
