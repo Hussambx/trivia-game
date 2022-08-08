4d521093bc788ec9258087e80c6da2e985134540
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-08 11:35:43 -0400

    Condtionally Rendered StartScreen.jsx using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Start from "./components/StartScreen"
+import React from "React"
 function App() {
   const [count, setCount] = useState(0)
+  const [isShown, SetShow] = React.useState(true)
 
+  function toggle(){
+    SetShow(prevState => !prevState)
+  }
+ 
   return (
     <div className="App">
-      <Start/>
+       {isShown &&<Start handleClick ={toggle}/>}
     </div>
   )
 }
