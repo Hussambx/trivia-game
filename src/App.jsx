ab5d9a767b8a2f483fb4bb94f70ac3c5bff82023
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-09 01:01:35 -0400

    Imported questions from API

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,20 @@ import React from "React"
 function App() {
   const [count, setCount] = useState(0)
   const [isShown, SetShow] = React.useState(true)
-
+  const [apidata, SetData] = React.useState([])
   function toggle(){
     SetShow(prevState => !prevState)
   }
+
+  React.useEffect(() => {
+        async function getQuestions() {
+            const res = await fetch("https://opentdb.com/api.php?amount=5")
+            const data = await res.json()
+            console.log(data.results[0]);
+        }
+        getQuestions()
+    }, [])
+    
  
   return (
     <div className="App">
