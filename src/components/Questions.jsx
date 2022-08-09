112ca9e474808f939d488d9291fd95b6ed0a3d8c
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-09 00:41:20 -0400

    Created Questions.jsx to house the questions

---
+++ src/components/Questions.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Questions(){
+return(
+    <>
+    <h2>Hi This Is My Test Question</h2>
+    <div className="options">
+    <button className="b11">Q1</button>
+    <button className="b12">Q1</button>
+    <button className="b13">Q1</button>
+    <button className="b14">Q1</button>
+    </div>
+    </>
+    
+
+)
+
+
+}
