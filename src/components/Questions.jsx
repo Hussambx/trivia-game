9513ebdd1ebc67a4033cc99f115e72da03ace4c1
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-10 10:52:38 -0400

    Randomly Shuffled Incoming Question Array

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 
-export default function Questions(){
+export default function Questions(props){
+    let buttons = ["b11","b12","b13","b14"]
+    let questions = [props.questiona,props.questionb,props.questionc,props.correctanswer]
+
+    for(let x =0; x<4; x++){
+        let rando =[Math.floor(Math.random() * 4)]
+        let first = questions[x];
+        let picked = questions[rando];
+        questions[rando] = first;
+        questions[x] = picked;
+        
+    }
+    console.log(questions);
+   
 return(
     <>
     <h2>Hi This Is My Test Question</h2>
