90b1a25dbda1e256fed63a6c6ccf3f984489cc16
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-10 12:17:26 -0400

    Added condtional rendering for if ANS selected

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function Questions(props){
     let buttons = ["b11","b12","b13","b14"]
+    let answer = props.correctanswer;
+    let selected = 5;
     let questions = [props.questiona,props.questionb,props.questionc,props.correctanswer]
 
     for(let x =0; x<4; x++){
@@ -12,16 +14,20 @@ export default function Questions(props){
         questions[x] = picked;
         
     }
-    console.log(questions);
+
+    function picked(num){
+        selected = num;
+        console.log(num);
+    }
    
 return(
     <>
     <h2>{props.mainquestion}</h2>
     <div className="options">
-    {questions[0]!=null ? <button className="b11">{questions[0]}</button>:console.log("no")}
-    {questions[1]!=null ?  <button className="b12">{questions[1]}</button>:console.log("no")}
-    {questions[2]!=null ? <button className="b13">{questions[2]}</button>:console.log("no")}
-    {questions[3]!=null? <button className="b14">{questions[3]}</button>:console.log("no")}
+    {questions[0]!=null ? <button className="b11" style={{background:selected==0?"DarkGrey":"LightGrey "}} onClick={() => picked(0)} >{questions[0]}</button>:console.log("no")}
+    {questions[1]!=null ?  <button className="b12" style={{background:selected==1?"DarkGrey":"LightGrey "}} onClick={() => picked(1)} >{questions[1]}</button>:console.log("no")}
+    {questions[2]!=null ? <button className="b13" style={{background:selected==2?"DarkGrey":"LightGrey "}} onClick={() => picked(2)} >{questions[2]}</button>:console.log("no")}
+    {questions[3]!=null? <button className="b14" style={{background:selected==3?"DarkGrey":"LightGrey "}} onClick={() => picked(3)} >{questions[3]}</button>:console.log("no")}
     </div>
     <hr></hr>
     </>
