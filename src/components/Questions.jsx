9bf3f74d10e9ddb25a9aac79d74e8d1018be00c3
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-11 11:15:48 -0400

    Moved Shuffling for loop 2 map function in app.jsx

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -3,35 +3,26 @@ import React from "react";
 export default function Questions(props){
     let buttons = ["b11","b12","b13","b14"]
     let answer = props.correctanswer;
+    let correct =false;
    const [selected, SetSelected] = React.useState(5);
-    let questions = [props.questiona,props.questionb,props.questionc,props.correctanswer]
-
-    React.useEffect(() => {
-        for(let x =0; x<4; x++){
-            let rando =[Math.floor(Math.random() * 4)]
-            let first = questions[x];
-            let picked = questions[rando];
-            questions[rando] = first;
-            questions[x] = picked;
-            
-        }
-    }, [buttons])
     
 
+    
+    console.log("answer is" +answer)
     function picked(num){
         SetSelected(num);
-        console.log(num);
-        console.log(props.id);
+        props.questions[selected] == answer?  correct=true:correct =false;
+        console.log(correct);
     }
    
 return(
     <>
     <h2>{props.mainquestion}</h2>
     <div className="options">
-    {questions[0]!=null ? <button className="b11" style={{background:selected==0?"DarkGrey":"LightGrey "}} onClick={() => picked(0)} >{questions[0]}</button>:console.log("no")}
-    {questions[1]!=null ?  <button className="b12" style={{background:selected==1?"DarkGrey":"LightGrey "}} onClick={() => picked(1)} >{questions[1]}</button>:console.log("no")}
-    {questions[2]!=null ? <button className="b13" style={{background:selected==2?"DarkGrey":"LightGrey "}} onClick={() => picked(2)} >{questions[2]}</button>:console.log("no")}
-    {questions[3]!=null? <button className="b14" style={{background:selected==3?"DarkGrey":"LightGrey "}} onClick={() => picked(3)} >{questions[3]}</button>:console.log("no")}
+    {props.questions[0]!=null && <button className="b11" style={{background:selected==0?"DarkGrey":"LightGrey "}} onClick={() => picked(0)} >{props.questions[0]}</button>}
+    {props.questions[1]!=null &&  <button className="b12" style={{background:selected==1?"DarkGrey":"LightGrey "}} onClick={() => picked(1)} >{props.questions[1]}</button>}
+    {props.questions[2]!=null && <button className="b13" style={{background:selected==2?"DarkGrey":"LightGrey "}} onClick={() => picked(2)} >{props.questions[2]}</button>}
+    {props.questions[3]!=null && <button className="b14" style={{background:selected==3?"DarkGrey":"LightGrey "}} onClick={() => picked(3)} >{props.questions[3]}</button>}
     </div>
     <hr></hr>
     </>
