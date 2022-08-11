a252179eff44eee6e586f99d911405a98ce2f747
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-11 11:16:35 -0400

    Added Key + Moved Shuffler into questionsdata

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,53 @@ function App() {
   const [count, setCount] = useState(0)
   const [isShown, SetShow] = React.useState(true)
   const [apidata, SetData] = React.useState([])
+  let keyx = 0;
   function toggle(){
     SetShow(prevState => !prevState)
   }
 
   React.useEffect(() => {
         async function getQuestions() {
-            const res = await fetch("https://opentdb.com/api.php?amount=5")
+            const res = await fetch("https://opentdb.com/api.php?amount=2")
             const data = await res.json()
             console.log(data.results);
             SetData(data.results);
         }
         getQuestions()
     }, [isShown])
-    
+  
+
+    function selectionhandler(){
+
+    }
+
+
+
     const questionsdata = apidata.map(travel=>{
       console.log(travel.correct_answer);
+
+      let questionsx = [travel.incorrect_answers[0],travel.incorrect_answers[1],travel.incorrect_answers[2],travel.correct_answer]
+
+      for(let x =0; x<4; x++){
+        let rando =[Math.floor(Math.random() * 4)]
+        if(rando==x){
+            rando =[Math.floor(Math.random() * 4)]
+        }
+        let first = questionsx[x];
+        let picked = questionsx[rando];
+        questionsx[rando] = first;
+        questionsx[x] = picked;
+    }
+
+
       return(
         <Questions
+        key = {keyx++}
+        id = {keyx}
         mainquestion = {travel.question}
         correctanswer = {travel.correct_answer}
-        questiona = {travel.incorrect_answers[0]}
-        questionb = {travel.incorrect_answers[1]}
-        questionc = {travel.incorrect_answers[2]}
+        questions = {questionsx}
+        
       />
       )
       
@@ -39,7 +63,12 @@ function App() {
   return (
     <div className="App">
        {isShown &&<Start handleClick ={toggle}/>}
-        {questionsdata}
+         {!isShown&& questionsdata}
+
+      <div button>
+        <button type='submit'> Check Answers</button>
+      </div>
+
     </div>
   )
 }
