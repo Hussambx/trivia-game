f0f450c7e08e31568f98b80485627b054b1b751d
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-11 20:39:35 -0400

    Updates qorder array accordingly using useeffect

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -14,7 +14,12 @@ export default function Questions(props){
 }, [props.handin])
 
 
-
+React.useEffect(() => {
+    if(props.handin==false){
+        SetOrder(prevState=> props.questions.slice())
+        console.log("adadadad" +qorder[0]);
+    }
+}, [props.questions])
       
 
 
@@ -35,10 +40,10 @@ return(
     <>
     <h2>{props.mainquestion}</h2>
     <div className="options">
-    {qorder[0]!=null && <button className="b11" style={{background:selected==0?"DarkGrey":"LightGrey "}} onClick={() => picked(0)} onMouseDown={props.questions[0]==props.correctanswer? props.handleClick:console.log("naj")} >{qorder[0]}</button>}
-    {qorder[1]!=null &&  <button className="b12" style={{background:selected==1?"DarkGrey":"LightGrey "}} onClick={() => picked(1)} onMouseDown={props.questions[1]==props.correctanswer? props.handleClick:console.log("naj")}>{qorder[1]}</button>}
-    {qorder[2]!=null && <button className="b13" style={{background:selected==2?"DarkGrey":"LightGrey "}} onClick={() => picked(2)} onMouseDown={props.questions[2]==props.correctanswer? props.handleClick:console.log("naj")}>{qorder[2]}</button>}
-    {qorder[3]!=null && <button className="b14" style={{background:selected==3?"DarkGrey":"LightGrey "}} onClick={() => picked(3)} onMouseDown={props.questions[3]==props.correctanswer? props.handleClick:console.log("naj")}>{qorder[3]}</button>}
+    {qorder[0]!=null && <button className="b11" style={{background:selected==0 && props.handin==false?"DarkGrey": props.handin==true && selected==0 && qorder[0]==props.correctanswer? "Green":props.handin==true && selected==0 && qorder[0]!=props.correctanswer?"Red": "LightGrey "}} onClick={() => picked(0)} onMouseDown={props.questions[0]==props.correctanswer? props.handleClick:console.log("naj")} >{qorder[0]}</button>}
+    {qorder[1]!=null &&  <button className="b12" style={{background:selected==1?"DarkGrey":"LightGrey "}} onClick={() => picked(1)} onMouseDown={qorder[1]==props.correctanswer? props.handleClick:console.log("naj")}>{qorder[1]}</button>}
+    {qorder[2]!=null && <button className="b13" style={{background:selected==2?"DarkGrey":"LightGrey "}} onClick={() => picked(2)} onMouseDown={qorder[2]==props.correctanswer? props.handleClick:console.log("naj")}>{qorder[2]}</button>}
+    {qorder[3]!=null && <button className="b14" style={{background:selected==3?"DarkGrey":"LightGrey "}} onClick={() => picked(3)} onMouseDown={qorder[3]==props.correctanswer? props.handleClick:console.log("naj")}>{qorder[3]}</button>}
     </div>
     <hr></hr>
     </>
