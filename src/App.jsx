e73e031c89007aa06a97d13515e445ac7afb28c4
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-08-12 12:52:02 -0400

    Cleaned Up Code

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Questions from "./components/Questions"
 import React from "React"
 
 function App() {
-  const [correctcount, setCount] = useState([false,false,false,false,false]) //This state is the array that keeps track of the amount of amount of selected correct answers 
+  const [correctcount, setCount] = React.useState([false,false,false,false,false]) //This state is the array that keeps track of the amount of amount of selected correct answers 
   const [isShown, SetShow] = React.useState(true) //This state updates once the user hits startquiz it switchs the screens to the quiz screen
   const [apidata, SetData] = React.useState([]) //This state saves the api data fetched from the server
   const [checkanswers,SetAnswers] = React.useState(false);  //This state updates to true once the player selects "check answers"
@@ -59,6 +59,7 @@ function App() {
     //This function goes through each instance of api data, it randomly sorts the questions and then using props its sent to the questions.jsx 
     const questionsdata = apidata.map(travel=>{
       let questionsx = [travel.incorrect_answers[0],travel.incorrect_answers[1],travel.incorrect_answers[2],travel.correct_answer]
+      
       //For statement below randomly sorts the questionsx array^
       //It basically loops through each instance of the array and basically sorts it using a random number within the array.length 
       for(let x =0; x<4; x++){
@@ -85,9 +86,8 @@ function App() {
         trackstats={correctcount}
       />
       )
-      
-    
      })
+
  //div className "ba" contains the check answers button
  //div class results contains the playagain button as well as the total #of correct answers 
  //each of those are conditonally render 
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,13 +1,10 @@
 import React from "react"
-import blob from '../assets/blobs@2x.png'
-import blob2 from "../assets/blobs.png"
 export default function StartScreen(props){
-    
 return(
     <>
     <h1>Quizzical</h1>
     <h4>Test Your Knowledge</h4>
-    <button type="button" onClick={props.handleClick}>Start Quiz</button>
+    <button className="b1" type="button" onClick={props.handleClick}>Start Quiz</button>
     </>
 )
 
